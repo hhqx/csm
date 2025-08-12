7a3af8867e185265a5469a3dbf00c154924ac08a
hqx <hqx>
2025-08-12 21:11:47 +0800

    add demo_loss.py and model weight desc json

--- tests/demo_loss.py
+++ tests/demo_loss.py
@@ -1,21 +1,19 @@
+
 import torch
-from transformers import CsmForConditionalGeneration, AutoProcessor, CsmProcessor
+from transformers import CsmForConditionalGeneration, AutoProcessor, CsmProcessor, MimiModel
 from datasets import load_dataset, Audio
 
 model_id = "/nfs/pretrained_models/csm-1b"
-device = "cuda" if torch.cuda.is_available() else "cpu"
+torch_device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# load the model and the processor
 processor: CsmProcessor = AutoProcessor.from_pretrained(model_id)
-model = CsmForConditionalGeneration.from_pretrained(model_id, device_map=device)
 
-# prepare the inputs
 ds = load_dataset("hf-internal-testing/dailytalk-dummy", split="train")
 # ensure the audio is 24kHz
 ds = ds.cast_column("audio", Audio(sampling_rate=24000))
-conversation = []
 
-# 1. context
+conversation = []
+# prepare a conversation with text and corresponding audio
 for text, audio, speaker_id in zip(ds[:4]["text"], ds[:4]["audio"], ds[:4]["speaker_id"]):
     conversation.append(
         {
@@ -24,15 +22,19 @@ for text, audio, speaker_id in zip(ds[:4]["text"], ds[:4]["audio"], ds[:4]["spea
         }
     )
 
-# 2. text prompt
-conversation.append({"role": f"{ds[4]['speaker_id']}", "content": [{"type": "text", "text": ds[4]["text"]}]})
-
 inputs = processor.apply_chat_template(
     conversation,
     tokenize=True,
     return_dict=True,
-).to(device)
+    output_labels=True,
+).to(torch_device)
 
-# infer the model
-audio = model.generate(**inputs, output_audio=True)
-processor.save_audio(audio, "outputs/example_with_context.wav")
+model = CsmForConditionalGeneration.from_pretrained(model_id, device_map=torch_device)
+output = model(**inputs)
+
+# model.codec_model: MimiModel = model.codec_model
+# output.loss.backward()
+
+print('loss:', output.loss)
+
+
